8b6b7f5f46004ca879d2b80255346c88bf5a7763
Unknown <odembowski@alteryx.com>
2017-11-02 09:03:06 -0700

    progress at innovation days
    progress at innovation days

---
+++ CURL7/src/test.bat
@@ -0,0 +1,2 @@
+URLHeadertypOutfile nameRequest
+"https://api.fantasydata.net/nfl/v2/xml/PlayerSeasonStats/2015REG""Ocp-Apim-Subscription-Key: 7f797abfc83c49aba7458746266abd28"GETDELETEME.xmlcurl -v -X GET "https://api.fantasydata.net/nfl/v2/xml/PlayerSeasonStats/2015REG" -H "Ocp-Apim-Subscription-Key: 7f797abfc83c49aba7458746266abd28" >> DELETEME.xml
